93c7cf2f25a538d5e4f680ba37c643f5063360fd
Alfredo Varas <luisvaras@hotmail.com>
2023-09-09 11:06:05 -0300

    Modify decimals

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def call_api(api_url):
         else:
             st.success("Resultado da análise: Baixa probabilidade de ter câncer")
 
-        st.warning(f"A probabilidade de câncer detectado é: {cancer_probability:.2%}")
+        st.warning(f"A probabilidade de câncer detectado é: {cancer_probability:.4%}")
     else:
         st.error("Ocorreu um erro ao processar a imagem.")
 
